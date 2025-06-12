93b408c6a07e2edaf5d5e0395adb19d0c22cfe15
RequiemTA <luigiman0412@gmail.com>
2025-06-12 01:17:00 -0500

    getting rid of the rest of the time freeze debuff

--- obj/Debug/net8.0/tmeogsQOL.AssemblyInfo.cs
+++ obj/Debug/net8.0/tmeogsQOL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tmeogsQOL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7cd8933819a285134cc22f2c1b3ffe27bf7fd19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a6adb961f1d33f33d610847279aa83af847362")]
 [assembly: System.Reflection.AssemblyProductAttribute("tmeogsQOL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tmeogsQOL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
